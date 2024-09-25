39c36160a49bba178018f862ad8a64ef370701dc
Kerem Semiz <kerox350@gmail.com>
2024-09-25 14:51:14 +0300

    website basics

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
     echo"It's really good!";
     // This is a comment
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
